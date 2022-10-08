eabfbd1f4034b2fd7608852f310a26102d3b75f4
Boris Zhao <boris1993@live.cn>
2022-10-08 23:53:26 +0800

    Fix entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,12 +4,14 @@ set -e
 
 CONFIG_FILE="/root/.GridcoinResearch/gridcoinresearch.conf"
 
-echo -e "rpcuser=grc_user" >> "${CONFIG_FILE}"
-echo -e "rpcpassword=grc_pass" >> "${CONFIG_FILE}"
-echo -e "printtoconsole=1" >> "${CONFIG_FILE}"
+if [ ! -f "$CONFIG_FILE" ]; then
+    echo -e "rpcuser=grc_user" >> "${CONFIG_FILE}"
+    echo -e "rpcpassword=grc_pass" >> "${CONFIG_FILE}"
+    echo -e "printtoconsole=1" >> "${CONFIG_FILE}"
 
-if [ -n "$EMAIL" ]; then
-    echo -e "email=${EMAIL}" >> "${CONFIG_FILE}"
+    if [ -n "$EMAIL" ]; then
+        echo -e "email=${EMAIL}" >> "${CONFIG_FILE}"
+    fi
 fi
 
 gridcoinresearchd
